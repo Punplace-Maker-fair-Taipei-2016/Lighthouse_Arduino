d0c9858bf6027fc8527c08fd24e4433dd55ce19a
WildfootTW <wild.foot.yee.tzwu@gmail.com>
2016-05-03 21:03:45 +0800

    add servo_ud

--- servo/servo.ino
+++ servo/servo.ino
@@ -4,9 +4,31 @@ Servo servo_under1;
 Servo servo_under2;
 Servo servo_ud;
 
+bool servo_ud_flag = 0;
+int servo_ud_max = 90, servo_ud_min = 10;
+
+
+int servo_ud_fun(int servo_ud_now)
+{
+    if(servo_ud_now == servo_ud_max)
+        servo_ud_flag = 1;
+    if(servo_ud_now == servo_ud_min)
+        servo_ud_flag = 0;
+
+    if(servo_ud_flag)
+    {
+        servo_ud_now -= 1;
+    }
+    else
+    {
+        servo_ud_now += 1;
+    }
+    return servo_ud_now;
+}
 
 void setup()
 {
+    Serial.begin(9600);
     servo_under1.attach(9);
     servo_under2.attach(10);
     servo_ud.attach(11);
@@ -21,8 +43,8 @@ void setup()
 
 void loop()
 {
-    
 
+    int servo_ud_now = 90;
     //*以角度控制
     for(int i = 10;i <= 170;i++)
     {
@@ -30,6 +52,8 @@ void loop()
         delay(20);
         servo_under2.write(i);
         delay(100);
+        servo_ud_now = servo_ud_fun(servo_ud_now);
+        servo_ud.write(servo_ud_now);
     }
     
     for(int i = 170;i >= 10;i--)
@@ -38,6 +62,8 @@ void loop()
         delay(20);
         servo_under2.write(i);
         delay(100);
+        servo_ud_now = servo_ud_fun(servo_ud_now);
+        servo_ud.write(servo_ud_now);
     }
 
     /*
