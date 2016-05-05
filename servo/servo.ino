65902085a166fc354c9806365fc4c0ed9f24f9ba
WildfootTW <wild.foot.yee.tzwu@gmail.com>
2016-05-05 17:26:05 +0800

    servo -> function

--- servo/servo.ino
+++ servo/servo.ino
@@ -9,25 +9,7 @@ int servo_ud_max = 90, servo_ud_min = 10;
 int servo_ud_now = 90;
 bool servo_under1_flag = 0, servo_under2_flag = 0;
 int servo_under_max = 170, servo_under_min = 10;
-int servo_under1_now = 90, servo_under2_now 90;
-
-int servo_ud_fun(int servo_ud_now)
-{
-    if(servo_ud_now == servo_ud_max)
-        servo_ud_flag = 1;
-    if(servo_ud_now == servo_ud_min)
-        servo_ud_flag = 0;
-
-    if(servo_ud_flag)
-    {
-        servo_ud_now -= 1;
-    }
-    else
-    {
-        servo_ud_now += 1;
-    }
-    return servo_ud_now;
-}
+int servo_under1_now = 90, servo_under2_now = 90;
 
 int servo_fun(int servo_now, int servo_max, int servo_min, bool *servo_flag)
 {
@@ -50,10 +32,11 @@ int servo_fun(int servo_now, int servo_max, int servo_min, bool *servo_flag)
 void setup()
 {
     Serial.begin(9600);
+
     servo_under1.attach(9);
     servo_under2.attach(10);
     servo_ud.attach(11);
-    //servo_under1.attach(9,500,2400);    //SG90脈衝寬度範圍是500到2400µs, pin 9 
+
     servo_under1.write(90);
     delay(20);
     servo_under2.write(90);
@@ -70,6 +53,7 @@ void loop()
     servo_under2_now = servo_fun(servo_under2_now, servo_under_max, servo_under_min, &servo_under2_flag);
     servo_under2.write(servo_under2_now);
 
-    servo_ud_now = servo_ud_fun(servo_ud_now);
+    servo_ud_now = servo_fun(servo_ud_now, servo_ud_max, servo_ud_min, &servo_ud_flag);
     servo_ud.write(servo_ud_now);
+
 }
